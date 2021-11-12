ff5b830f359786dc64ce3b57658c647cc06915fb
Luz Marina Galindo <90713851+LuzMa20@users.noreply.github.com>
2021-11-12 15:31:56 -0500

    Registro de Vendedores/todas las panallas

---
+++ app/src/main/java/com/luzmag/programacionmoviles/SellerRegister.kt
@@ -0,0 +1,11 @@
+package com.luzmag.programacionmoviles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SellerRegister : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_seller_register)
+    }
+}
